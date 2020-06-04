963469dc6fa3e994be1602c86253401875a8bbd0
Nadia chrispens <nadiachrispens2@gmail.com>
2020-06-04 11:41:13 -0300

    base de dato

---
+++ bd.sql
@@ -0,0 +1,11 @@
+create database puntosDeInteres;
+
+create table puntoDeInteres(
+    nombre text,
+    descripcion text,
+    categoria text,
+    direccion text
+);
+
+insert into puntoDeInteres values
+    ('bartolo', 'bar resto', 'comida', 'san martin y 3 de febrero');
