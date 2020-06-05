b76561a9a92c2225349c6a85323af7e0dbda6a97
Nadia chrispens <nadiachrispens2@gmail.com>
2020-06-05 11:49:02 -0300

    no se q poner

--- src/controladores/index.controlers.js
+++ src/controladores/index.controlers.js
@@ -12,39 +12,39 @@ const pool= new Pool({
 const getPDI = (req,res) => {
     const respuesta = pool.query('SELECT * FROM puntodeinteres')
     .then(respuesta => res.status(200).json(respuesta.rows));
-}
+};
 
 const createPDI = (req,res) => {
-    const {nombre, descripcion, categoria, direccion} = req.body;
-    const respuesta = pool.query('INSERT INTO puntodeinteres {nombre, descripcion, categoria, direccion} VALUES ($1, $2, $3,$4)', [nombre, descripcion, categoria, direccion]);
-    then(respuesta => console.log(respuesta))
-    then(res.json({
+    const {id, nombre, descripcion, categoria, direccion} = req.body;
+    const respuesta = pool.query('INSERT INTO puntodeinteres {id, nombre, descripcion, categoria, direccion} VALUES ($1, $2, $3,$4, $5)', [id, nombre, descripcion, categoria, direccion])
+    .then(respuesta => console.log(respuesta))
+    .then(res.json({
         message: 'Punto de interes agregado con exito',
         body: {
-                puntodeinteres: (nombre, descripcion, categoria, direccion)
+                puntodeinteres: (id, nombre, descripcion, categoria, direccion)
               }
-    }));
+    }))
 };
 
 const deletePDI = (req,res) => {
     const id= req.params.id
     const respuesta = pool.query('DELETE FROM puntodeinteres WHERE id = $1',[id] )
-    then(respuesta => console.log(respuesta));
-    then(res.json(`Punto de interes ${id} eliminado con exito `));
-}
+    .then(respuesta => console.log(respuesta))
+    .then(res.json(`Punto de interes ${id} eliminado con exito `));
+};
 
 const getPDIByID = (req,res) => {
     const id =req.params.id;
    const respuesta = pool.query('SELECT * FROM puntodeinteres WHERE id = $1', [id])
-   then(respuesta => res.json(respuesta.rows));
+   .then(respuesta => res.json(respuesta.rows));
 };
 
 const updatePDI = (req,res) => {
     const id = req.params.id;
     const {nombre, categoria} = req.body;
-    const respuesta = pool.query('UPDATE puntodeinteres SET nombre=$1, categoria=$2 WHERE id=$3', [nombre, categoria ,id])
-    then(respuesta => console.log(respuesta));
-    then(res.json(`Punto de interes ${id} actualizado con exito `));
+    const respuesta = pool.query('UPDATE puntodeinteres SET nombre=$1, categoria=$2 WHERE id=$3', [nombre, categoria, id])
+    .then(respuesta => console.log(respuesta))
+    .then(res.json(`Punto de interes ${id} actualizado con exito `));
 };
 
 
--- src/rutas/index.js
+++ src/rutas/index.js
@@ -2,11 +2,11 @@ const {Router} = require('express');
 const router = Router();
 const {getPDI, createPDI, getPDIByID, deletePDI, updatePDI} =require('../controladores/index.controlers');
 
-//router.get('/', getPDI);
-router.post('/pdi', createPDI);
-//router.get('/pdi/:id', getPDIByID);
-//router.delete('/pdi/:id', deletePDI);
-//router.put('/pdi/:id', updatePDI);
+router.get('/', getPDI);
+router.post('/', createPDI);
+router.get('/:id', getPDIByID);
+router.delete('/:id', deletePDI);
+router.put('/:id', updatePDI);
 
 
 
